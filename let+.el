5a9c180211f20b0eb9837f9e9d126aeafb4dd4b6
Keith Wygant <kawygant@ncsu.edu>
2017-07-11 13:55:44 -0400

    got things working.

--- let+.el
+++ let+.el
@@ -2,27 +2,33 @@
 (mapc #'require [cl-macs seq])
 
 (defmacro let+ (BINDINGS &rest BODY)
-  "Imagine BINDINGS as a big `setf' body that does the right thing with lists, functions, etc."
+  "Imagine BINDINGS as a big `setf' body that does the right thing with lists, functions, etc.
+
+Example:
+\(let+ ((a b) '(1 2)
+       \foo ((x y) (+ x y)))
+  \(foo a b))
+=> 3"
   (cl-labels
-      ((let-helper
-        (BINDINGS)
-        (if BINDINGS
-            (seq-let (var val &rest bs) BINDINGS
-               (cond
-                ((sequencep var)
-                 ;; Pattern-match sequence.
-                 `(seq-let ,var ,val
-                    ,(let-helper bs)))
-                ((and (consp val)
-                      (listp (car val))
-                  ;; Bind procedure.
-                  `(cl-labels ((,var . ,val))
-                     ,(let-helper bs)))
-                 (t
-                  ;; Bind as usual.
-                  `(let ((,var ,val))
-                       ,(let-helper bs))))))
-            `(progn . ,BODY))))
-    (let-helper BINDINGS)))
+      ((sublet
+        (var val)
+        (cond
+         ((sequencep var)
+          ;; Pattern-match sequence.
+          `(seq-let ,var ,val))
+         ((and (consp val)
+               (listp (car val))
+           ;; Bind procedure.
+           `(cl-labels ((,var . ,val)))))
+         (t `(let ((,var ,val))))))
+
+       (combind
+        (bindings)
+        (if bindings
+            (seq-let (var val &rest brest) bindings
+              (let ((sublet (sublet var val)))
+                (append sublet `(,(combind brest)))))
+          `(progn . ,BODY))))
+    (combind BINDINGS)))
 
 (provide 'let+)
